6f73a400c933b44e8639b5eb1fb4372a526aeea6
Pattaradanai Chabang <ktop@iMac-khxng-Pattaradanai.local>
2024-08-09 13:40:21 +0700

    Ex 1-4 Finish

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let addRess = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
+const restaurantName = "Chef's Table by Chef James";
+const totalBillAmount = 15000;
+const totalRestaurantsOpen = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+restaurantName = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalBillAmount = 5000
+totalRestaurantsOpen = 2500
